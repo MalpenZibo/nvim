d48c370eedc43e3c4cd73f4caa332d8aae47089e
Simone Camito <zibo.camito@gmail.com>
2023-09-04 18:15:41 +0200

    fix lsp and copilot keybinding + minor toggle term fixes

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -19,6 +19,22 @@ lsp.configure("rust_analyzer", {
 -- Fix Undefined global 'vim'
 lsp.nvim_workspace()
 
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+	["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
+})
+
+cmp_mappings["<Tab>"] = nil
+cmp_mappings["<S-Tab>"] = nil
+
+lsp.setup_nvim_cmp({
+	mapping = cmp_mappings,
+})
+
 lsp.on_attach(function(client, bufnr)
 	-- see :help lsp-zero-keybindings
 	-- to learn the available actions
