979d2bd3876a4f005631bfa660b6a265b326d08f
Simone Camito <zibo.camito@gmail.com>
2024-07-09 10:10:24 +0200

    adapt to tmux

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -150,92 +150,36 @@ return { {
     end
   },
   {
-    'akinsho/bufferline.nvim',
-    dependencies = 'nvim-tree/nvim-web-devicons',
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
     config = function()
-      local bufferline = require("bufferline")
-      bufferline.setup {
-        options = {
-          diagnostics = "nvim_lsp",
-          hover = {
-            enabled = true,
-            delay = 150,
-            reveal = { 'close' }
-          }
-        }
-      }
-
       local map = vim.api.nvim_set_keymap
       local opts = { noremap = true, silent = true }
 
       -- Move to previous/next
-      map("n", "<A-,>", "<Cmd>BufferLineCyclePrev<CR>", opts)
-      map("n", "<A-.>", "<Cmd>BufferLineCycleNext<CR>", opts)
+      map("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
+      map("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
       -- Re-order to previous/next
-      map("n", "<A-<>", "<Cmd>BufferLineMovePrev<CR>", opts)
-      map("n", "<A->>", "<Cmd>BufferLineMoveNext<CR>", opts)
+      map("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", opts)
+      map("n", "<A->>", "<Cmd>BufferMoveNext<CR>", opts)
       -- Goto buffer in position...
-      map("n", "<A-1>", "<Cmd>BufferLineGoToBuffer 1<CR>", opts)
-      map("n", "<A-2>", "<Cmd>BufferLineGoToBuffer 2<CR>", opts)
-      map("n", "<A-3>", "<Cmd>BufferLineGoToBuffer 3<CR>", opts)
-      map("n", "<A-4>", "<Cmd>BufferLineGoToBuffer 4<CR>", opts)
-      map("n", "<A-5>", "<Cmd>BufferLineGoToBuffer 5<CR>", opts)
-      map("n", "<A-6>", "<Cmd>BufferLineGoToBuffer 6<CR>", opts)
-      map("n", "<A-7>", "<Cmd>BufferLineGoToBuffer 7<CR>", opts)
-      map("n", "<A-8>", "<Cmd>BufferLineGoToBuffer 8<CR>", opts)
-      map("n", "<A-9>", "<Cmd>BufferLineGoToBuffer 9<CR>", opts)
-      map("n", "<A-0>", "<Cmd>BufferLineGoToBuffer -1<CR>", opts)
+      map("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", opts)
+      map("n", "<A-2>", "<Cmd>BufferGoto 2<CR>", opts)
+      map("n", "<A-3>", "<Cmd>BufferGoto 3<CR>", opts)
+      map("n", "<A-4>", "<Cmd>BufferGoto 4<CR>", opts)
+      map("n", "<A-5>", "<Cmd>BufferGoto 5<CR>", opts)
+      map("n", "<A-6>", "<Cmd>BufferGoto 6<CR>", opts)
+      map("n", "<A-7>", "<Cmd>BufferGoto 7<CR>", opts)
+      map("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", opts)
+      map("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", opts)
+      map("n", "<A-0>", "<Cmd>BufferLast<CR>", opts)
+      -- Close buffer
+      map("n", "<A-q>", "<Cmd>BufferClose<CR>", opts)
+      map("n", "<A-Q>", "<Cmd>BufferCloseAllButCurrent<CR>", opts)
       -- Magic buffer-picking mode
-      map("n", "<A-p>", "<Cmd>BufferLinePick<CR>", opts)
+      map("n", "<A-p>", "<Cmd>BufferPick<CR>", opts)
     end
   },
-  {
-    'ojroques/nvim-bufdel',
-    config = function()
-      require('bufdel').setup {
-        next = 'tabs',
-        quit = false, -- quit Neovim when last buffer is closed
-      }
-
-      local map = vim.api.nvim_set_keymap
-      local opts = { noremap = true, silent = true }
-
-      -- Close buffer
-      map("n", "<A-q>", "<Cmd>BufDel<CR>", opts)
-      map("n", "<A-Q>", "<Cmd>BufDelOthers<CR>", opts)
-    end
-  }
-  -- {
-  --   'romgrk/barbar.nvim',
-  --   dependencies = {
-  --     'nvim-tree/nvim-web-devicons',
-  --   },
-  --   config = function()
-  --     local map = vim.api.nvim_set_keymap
-  --     local opts = { noremap = true, silent = true }
-  --
-  --     -- Move to previous/next
-  --     map("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
-  --     map("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
-  --     -- Re-order to previous/next
-  --     map("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", opts)
-  --     map("n", "<A->>", "<Cmd>BufferMoveNext<CR>", opts)
-  --     -- Goto buffer in position...
-  --     map("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", opts)
-  --     map("n", "<A-2>", "<Cmd>BufferGoto 2<CR>", opts)
-  --     map("n", "<A-3>", "<Cmd>BufferGoto 3<CR>", opts)
-  --     map("n", "<A-4>", "<Cmd>BufferGoto 4<CR>", opts)
-  --     map("n", "<A-5>", "<Cmd>BufferGoto 5<CR>", opts)
-  --     map("n", "<A-6>", "<Cmd>BufferGoto 6<CR>", opts)
-  --     map("n", "<A-7>", "<Cmd>BufferGoto 7<CR>", opts)
-  --     map("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", opts)
-  --     map("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", opts)
-  --     map("n", "<A-0>", "<Cmd>BufferLast<CR>", opts)
-  --     -- Close buffer
-  --     map("n", "<A-q>", "<Cmd>BufferClose<CR>", opts)
-  --     map("n", "<A-Q>", "<Cmd>BufferCloseAllButCurrent<CR>", opts)
-  --     -- Magic buffer-picking mode
-  --     map("n", "<A-p>", "<Cmd>BufferPick<CR>", opts)
-  --   end
-  -- },
 }
--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -6,30 +6,6 @@ return {
     end
   },
   {
-    "akinsho/toggleterm.nvim",
-    config = function()
-      require("toggleterm").setup({
-        open_mapping = [[<C-\>]],
-        direction = "vertical",
-        shade_terminals = true,
-        size = 60,
-      })
-
-      function _G.set_terminal_keymaps()
-        local opts = { buffer = 0 }
-        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-        vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-        vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-      end
-
-      vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
-    end
-  },
-  {
     "numTostr/Comment.nvim",
     config = function()
       require("Comment").setup()
@@ -48,13 +24,30 @@ return {
         log_level = "error",
 
         cwd_change_handling = {
-          restore_upcoming_session = true, -- already the default, no need to specify like this, only here as an example
-          pre_cwd_changed_hook = nil,  -- already the default, no need to specify like this, only here as an example
+          restore_upcoming_session = true,   -- already the default, no need to specify like this, only here as an example
+          pre_cwd_changed_hook = nil,        -- already the default, no need to specify like this, only here as an example
           post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
-            require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
+            require("lualine").refresh()     -- refresh lualine so the new session name is displayed in the status bar
           end,
         },
       });
     end
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
   }
 }
